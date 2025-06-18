b53d0ee02ec9c1ee2a47cd2b51d6e8506ac9b0f3
SinckCode <soyangeldavid1@gmail.com>
2025-06-18 12:43:06 -0600

    Aceptando el CORS

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # 👈 agregado
 from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
 from routes.libro import libro_router
 from routes.user import user_router
-
 from models import libro
 
 app = FastAPI(
     title="LibreriaApi - Sistema de Gestión de Libros",
     version="1.0.0"
+)
+
+# 👇 agrega esto
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.add_middleware(ErrorHandler)
